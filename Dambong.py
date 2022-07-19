aa9db446bf1f97d76c92062911483cb73180799f
letiennghia <80113864+letiennghia@users.noreply.github.com>
2022-07-19 15:03:27 +0700

    Update Dambong.py

--- Dambong.py
+++ Dambong.py
@@ -3,35 +3,57 @@ import torch.nn as nn  # All neural network modules, nn.Linear, nn.Conv2d, Batch
 import torch.optim as optim  # For all Optimization algorithms, SGD, Adam, etc.
 import torchvision.transforms as transforms  # Transformations we can perform on our dataset
 import torchvision
+from PIL import Image, ImageEnhance
+import math
 import os
 import pandas as pd
 from skimage import io
+
 from torch.utils.data import (
     Dataset,
     DataLoader,
 )  # Gives easier dataset managment and creates mini batches
-
-
-class CatsAndDogsDataset(Dataset):
-    def __init__(self, csv_file, root_dir, transform=None):
-        self.annotations = pd.read_csv(csv_file)
-        self.root_dir = root_dir
+transform = transforms.Compose([
+    transforms.Resize((224, 224)), 
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]) # to tensor before normalize
+])
+def load_image(file):
+    return Image.open(file)
+def image_path(root, basename, extension):
+    return os.path.join(root, f'{basename}{extension}')
+os.listdir('../input/hsgs-hackathon2022/train_data/Train_labels')
+class CustomImageDataset(Dataset):
+    def __init__(self, img_dir, label_dir, transform=transform):
+        video_files = os.listdir(img_dir)
+        self.labels = list()
+        self.imgs = list()
+        for file_name in video_files: 
+            cur_csv = pd.read_csv(os.path.join(label_dir, file_name+'.csv'))
+            cur_len = len(cur_csv.index)
+            for itr in range(cur_len):
+                img_file_dir = os.path.join(os.path.join(img_dir, file_name), cur_csv.iloc[itr, 0] + '.PNG')
+                self.imgs.append(img_file_dir) # image directory
+                self.labels.append([cur_csv.iloc[itr, 0], cur_csv.iloc[itr, 1]]) # label
+        self.img_dir = img_dir
+        self.label_dir = label_dir
         self.transform = transform
-
-    def __len__(self):
-        return len(self.annotations)
-
+            
     def __getitem__(self, index):
-        img_path = os.path.join(self.root_dir, self.annotations.iloc[index, 0])
-        image = io.imread(img_path)
-        y_label = torch.tensor(int(self.annotations.iloc[index, 1]))
-
-        if self.transform:
+        with open(self.imgs[index], 'rb') as f:
+            image = load_image(f).convert('RGB')
+        label = self.labels[index][1]
+        filename = self.labels[index][0]
+        
+#         image, label = self.spm_transform(image, label)
+        
+        if self.transform is not None:
             image = self.transform(image)
 
-        return (image, y_label)
+        return image, label
 
-
+    def __len__(self):
+        return len(self.labels)
 # Set device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -39,22 +61,22 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 in_channel = 3
 num_classes = 2
 learning_rate = 1e-3
-batch_size = 32
-num_epochs = 10
+batch_size = 64
+num_epochs = 200
 
 # Load Data
-dataset = CatsAndDogsDataset(
-    csv_file="cats_dogs.csv",
-    root_dir="cats_dogs_resized",
-    transform=transforms.ToTensor(),
-)
 
+dataset = CustomImageDataset(
+    img_dir='../input/hsgs-hackathon2022/train_data/Train',
+    label_dir='../input/hsgs-hackathon2022/train_data/Train_labels'
+)
 # Dataset is actually a lot larger ~25k images, just took out 10 pictures
 # to upload to Github. It's enough to understand the structure and scale
 # if you got more images.
-train_set, test_set = torch.utils.data.random_split(dataset, [5, 5])
+n_train = math.floor(len(dataset)) 
+train_set=dataset
 train_loader = DataLoader(dataset=train_set, batch_size=batch_size, shuffle=True)
-test_loader = DataLoader(dataset=test_set, batch_size=batch_size, shuffle=True)
+
 
 # Model
 model = torchvision.models.googlenet(pretrained=True)
